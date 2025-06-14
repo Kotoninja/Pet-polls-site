16a443cbf2335efa1f4e5dc8ad3d17254d30aa8c
artem <SS4T46@yandex.ru>
2025-06-14 22:49:39 +0300

    add template auth

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+
+def login(request):
+    return render(request,"users/login.html",{})
+
+
+def register(request):
+    return render(request,"users/register.html")
