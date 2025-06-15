59a7186948970819f822efcb4e09b0d231036a18
artem <SS4T46@yandex.ru>
2025-06-15 19:28:51 +0300

    register code

--- users/views.py
+++ users/views.py
@@ -1,11 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth.hashers import make_password
+
+User = get_user_model()
 
 
 def login(request):
-    return render(request,"users/login.html",{})
+    if request.POST:
+        user_username = request.POST["username"]
+        user_password = request.POST["password"]
+        return HttpResponse(f"{user_username},{user_password}")
+    return render(request, "users/login.html", {})
 
 
 def register(request):
-    return render(request,"users/register.html")
+    context = {}
+    if request.POST:
+        user_username = request.POST["username"]
+        user_email = request.POST["email"]
+
+        data = f"{user_username},{user_email},{request.POST['password']},{request.POST['repassword']}"
+
+        # return HttpResponse(f"{User.objects.filter(email = user_email).exists()}")
+        if request.POST["password"] != request.POST["repassword"]:
+            return HttpResponse("repeat password is incorrect")
+        elif User.objects.filter(email=user_email).exists():
+            return HttpResponse("Аn account with this email address exists")
+        else:
+            return HttpResponse("user created")
+    return render(request, "users/register.html",context=context)
