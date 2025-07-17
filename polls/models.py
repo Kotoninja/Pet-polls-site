606e3dd0f0c5b17711e743d08cf6cbfd4796efcd
artem <SS4T46@yandex.ru>
2025-07-17 14:16:22 +0300

    add tag model

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=50)
     question_votes = models.IntegerField(default=0)
     question_author = models.CharField(max_length=20, default="Anonymous")
+    tags = models.ManyToManyField("Tag", blank=True)
 
     def __str__(self):
         return f"user - {self.question_author}; question - {self.question_text}; count of votes - {self.question_votes}"
@@ -20,3 +21,10 @@ class Choice(models.Model):
 
     # def get_absolute_url(self):
     #     return reverse("_detail", kwargs={"pk": self.pk})
+
+
+class Tag(models.Model):
+    tag_text = models.CharField(max_length=25)
+
+    def __str__(self) -> str:
+        return self.tag_text
